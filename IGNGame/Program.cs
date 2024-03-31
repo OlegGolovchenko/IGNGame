c445e7e60d2dafb08ff419d342a425c30cc27429
Oleg Golovchenko <igrok_be@hotmail.com>
2024-03-31 21:46:27 +0200

    fixed libraries for linux

--- IGNGame/Program.cs
+++ IGNGame/Program.cs
@@ -6,6 +6,9 @@ class Program
 {
     public static void Main(string[] args)
     {
+#if WINDOWS
+        Console.WriteLine("windows platform detected!");
+#endif
         Window window = new Window("IgRok-NET Game v0.0.1", args);
         window.Render();
     }
