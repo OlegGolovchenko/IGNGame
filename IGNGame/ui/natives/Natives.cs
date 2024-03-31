5e506a236474bad36d3b890ddeda5c870a6f1607
Oleg Golovchenko <IgRoK_be@hotmail.com>
2024-03-31 21:25:03 +0200

    adding library name for linux and mac

--- IGNGame/ui/natives/Natives.cs
+++ IGNGame/ui/natives/Natives.cs
@@ -13,39 +13,44 @@ namespace org.igrok_net.game.ui.natives
     internal static class Natives
     {
         #region "GLFW Functions"
+#if WINDOWS
+        public const string GLFW_LIB = "glfw3";
+#else
+        public const string GLFW_LIB = "libglfw3";
+#endif
         public const int GLFW_TRUE = 1;
         public const int GLFW_FALSE = 0;
         public const int GLFW_CONTEXT_VERSION_MAJOR = 0x00022002;
         public const int GLFW_CONTEXT_VERSION_MINOR = 0x00022003;
 
-        [DllImport("glfw3")]
+        [DllImport(GLFW_LIB)]
         public static extern int glfwInit();
-        [DllImport("glfw3")]
+        [DllImport(GLFW_LIB)]
         public static extern void glfwTerminate();
-        [DllImport("glfw3")]
+        [DllImport(GLFW_LIB)]
         [return: MarshalAs(UnmanagedType.FunctionPtr)]
         public static extern GLFWErrorFunc glfwSetErrorCallback([MarshalAs(UnmanagedType.FunctionPtr)] GLFWErrorFunc errorFunc);
-        [DllImport("glfw3")]
+        [DllImport(GLFW_LIB)]
         public static extern IntPtr glfwCreateWindow([MarshalAs(UnmanagedType.I4)] int width, [MarshalAs(UnmanagedType.I4)] int height, [MarshalAs(UnmanagedType.LPStr)] string title, IntPtr monitor, IntPtr share);
-        [DllImport("glfw3")]
+        [DllImport(GLFW_LIB)]
         public static extern void glfwWindowHint([MarshalAs(UnmanagedType.I4)] int hint, [MarshalAs(UnmanagedType.I4)] int value);
-        [DllImport("glfw3")]
+        [DllImport(GLFW_LIB)]
         public static extern void glfwMakeContextCurrent(IntPtr window);
-        [DllImport("glfw3")]
+        [DllImport(GLFW_LIB)]
         [return: MarshalAs(UnmanagedType.I4)]
         public static extern int glfwWindowShouldClose(IntPtr window);
-        [DllImport("glfw3")]
+        [DllImport(GLFW_LIB)]
         public static extern void glfwDestroyWindow(IntPtr window);
-        [DllImport("glfw3")]
+        [DllImport(GLFW_LIB)]
         public static extern void glfwPollEvents();
-        [DllImport("glfw3")]
+        [DllImport(GLFW_LIB)]
         public static extern void glfwSwapBuffers(IntPtr window);
-        [DllImport("glfw3")]
+        [DllImport(GLFW_LIB)]
         [return: MarshalAs(UnmanagedType.FunctionPtr)]
         public static extern GLFWkeyfun glfwSetKeyCallback(IntPtr window, [MarshalAs(UnmanagedType.FunctionPtr)] GLFWkeyfun callback);
-        [DllImport("glfw3")]
+        [DllImport(GLFW_LIB)]
         public static extern void glfwGetFramebufferSize(IntPtr window, [MarshalAs(UnmanagedType.I4)] ref int width, [MarshalAs(UnmanagedType.I4)] ref int height);
-        #endregion
+#endregion
 
         #region "GLUT Functions"
         [DllImport("freeglut")]
