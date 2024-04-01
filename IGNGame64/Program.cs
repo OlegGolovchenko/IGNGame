d4c0f3ab6bcbba1b33ddf5674491af31ea66170f
Oleg Golovchenko <igrok_be@hotmail.com>
2024-04-01 15:46:44 +0200

    removed unneeded check

--- IGNGame64/Program.cs
+++ IGNGame64/Program.cs
@@ -7,7 +7,6 @@ namespace IGNGame64
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Natives.getGlutTimesNewRoman24());
             Window window = new Window("IgRok-NET Game v0.0.1", args);
             window.Render();
         }
